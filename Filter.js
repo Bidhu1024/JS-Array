1941207ffeab2f63fccf25e5d420213e92e85100
Bidhu1024 <bidhu1024av@gmail.com>
2023-02-26 23:01:15 +0530

    Cont tom

---
+++ Filter.js
@@ -0,0 +1,36 @@
+// const number  = [1,2,3,4,5,6,7,8,9]
+
+// const arr = number.filter((item)=>{
+//     return item % 2 === 0;
+    
+// })
+
+// const person = [
+//     {
+//         name:'Gudu',
+//         age:24
+//     },
+//     {
+//         name:'Rinky',
+//         age:24
+//     },
+//     {
+//         name:'Sikuna',
+//         age:20
+//     }
+
+// ]
+
+// //filter out the people above 22
+
+// const people = person.filter((item)=>{
+//     return item.age > 22;
+    
+// })
+
+
+const number = [1,2,3,4,3,2,5,7,6,,8,5,4,3,2,3,5,6,7,89,6,4,3,4,5,6,7,8,5,4,3,2,1,0,0,0,8,7,5,4,3,2,1]
+const rmdup  = number.filter((item, i, arr)=>{
+    return item
+})
+console.log(people)
